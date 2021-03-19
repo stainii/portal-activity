44832429e43ddb97a6ba0b14284c9bc5c1bcde49
stainii <stijnhooft@hotmail.com>
2021-03-19 18:21:41 +0100

    remove unused listener to event queue

--- src/main/java/be/stijnhooft/portal/activity/messaging/EventTopic.java
+++ src/main/java/be/stijnhooft/portal/activity/messaging/EventTopic.java
@@ -1,16 +1,11 @@
 package be.stijnhooft.portal.activity.messaging;
 
-import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.messaging.MessageChannel;
 
 public interface EventTopic {
 
-    String INPUT = "readFromEventTopic";
     String OUTPUT = "writeToEventTopic";
-
-    @Input(INPUT)
-    MessageChannel readFromEventTopic();
 
     @Output(OUTPUT)
     MessageChannel writeToEventTopic();
